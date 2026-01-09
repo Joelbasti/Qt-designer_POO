d659c62378e294cb08347cd98dd615ccf22c49f0
Joelbasti <joelbasti23@gmail.com>
2026-01-09 08:58:02 -0500

    Add files via upload

---
+++ persona.py
@@ -0,0 +1,11 @@
+from PySide6 import QtGui
+from PySide6.QtWidgets import QMainWindow
+
+
+class PersonaServicio(QMainWindow):
+    '''
+    Clase que genera la logica de la clase PersonaServicio
+    '''
+    def __init__(self):
+        super(PersonaServicio, self).__init__()
+
